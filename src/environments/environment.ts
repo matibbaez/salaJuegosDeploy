bfbb46de37afc8cbd974cb1204b3b3255ec08ded
matibbaez <matiascaneda3@gmail.com>
2025-10-07 10:26:45 -0300

    Fix: Asegurar que environment files esten disponibles para Vercel build

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://hqbqsweovhepvzbtnfaz.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxYnFzd2VvdmhlcHZ6YnRuZmF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NDgzMTksImV4cCI6MjA3NTMyNDMxOX0.pV6ue0JNKvYFWG-1fAYY1GoR-6W3o1fEvowIDb54txk'
+};
